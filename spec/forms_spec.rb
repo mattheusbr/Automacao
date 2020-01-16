9ec3bf7f04022fd9a8826373d7a649dff7dbaf18
Matheus Ferreira Bruno <matheus.bruno@benner.com.br>
2020-01-16 13:37:19 -0300

    Auto Login

--- spec/forms_spec.rb
+++ spec/forms_spec.rb
@@ -1,13 +1,27 @@
 describe 'Forms' do
-    it 'Logado' do
-       visit 'http://mga-apl044/QualidadeAG_CorrenteOld'
-       fill_in 'pu', with: 'programador'
-       fill_in 'pp', with: 'benner'
+    it 'Login no Site' do
+        visit 'http://training-wheels-protocol.herokuapp.com/login'
+        fill_in 'username', with: 'stark'
+        fill_in 'password', with: 'jarvis!'
 
-       click_button 'Entrar'
-       expect(page).to have_content 'Benner WES 2006.181.1.81teste' 
+        click_button 'Login'
+
+        #Pergunta se o elemento #flash está visivel após o login
+        expect(find('#flash').visible?).to be true
+        
+        #Verifica se o texto do elemento mostrado na pagina contem essa parte
+        expect(find('#flash').text).to include 'Olá, Tony Stark. Você acessou a área logada!'
+
+        #Verifica se o texto no elemento contem essa parte
+        expect(find("#flash")).to have_content 'Olá, Tony Stark. Você acessou a área logada!'
+    
+    
+        #have_content | include (da o mesmo resultado de verificação)
 
     end
 end
 
+
+#===comentarios===#
 # Get.Elementbyxpach(//*[@id="rp1_edt"]/table/tbody/tr[2]/td/form/table/tbody/tr[1]/td[2]/input). click_button\
+#expect(page).to have_content 'Benner WES 2006.181.1.81teste' 
--- spec/hello_spec.rb
+++
@@ -1,7 +0,0 @@
-describe 'Hello World - 1º estudo automação' do
-
-    it 'Visitar a página' do
-        visit 'http://mga-apl044/QualidadeAG_CorrenteOld'
-        expect(page.title).to eql 'Login' 
-    end
-end
