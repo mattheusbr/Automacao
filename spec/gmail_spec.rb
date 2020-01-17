421f02697d83d3d40c2e2afec645adeea5ede26d
Matheus Ferreira Bruno <matheusferreirabruno@hotmail.com>
2020-01-17 18:03:00 -0300

    'Gmail'

---
+++ spec/gmail_spec.rb
@@ -0,0 +1,51 @@
+require 'selenium-webdriver'
+selenium: :webdriver: :Chrome.driver_path="C:/chromedriver.exe"
+@driver = Selenium: :WebDriver.for :chrome
+
+
+describe 'Forms' do
+    it 'Excluir Emails' do
+        visit 'https://mail.google.com/mail/u/0/?tab=wm&ogbl&zx=vlndz4h7blg3#inbox'
+        
+        fill_in 'identifier', with: 'automacaoteste18@gmail.com'
+
+        Elemento= find("#identifierNext")
+        Elemento.click
+
+        fill_in 'password', with: 'benner123'
+        
+        Elemento = find("#passwordNext") 
+        Elemento.click
+    
+        expect(page.title).to eql 'Gmail' 
+        @driver.find_element(:id, ":2r").submit
+    
+
+
+
+        #click_on(class: 'ZFr60d CeoRYc')
+        #find_element(:xpath,'//*[@id="identifierNext"]/span').click()
+        
+        
+        #fill_in 'password', with: '@'
+        #click_button 'identifierNext'
+
+        #Pergunta se o elemento #flash está visivel após o login
+        #expect(find('#:2r').visible?).to be true
+        
+        #Verifica se o texto do elemento mostrado na pagina contem essa parte
+        #expect(find('#flash').text).to include 'Olá, Tony Stark. Você acessou a área logada!'
+
+        #Verifica se o texto no elemento contem essa parte
+        #expect(find("#flash")).to have_content 'Olá, Tony Stark. Você acessou a área logada!'
+    
+    
+        #have_content | include (da o mesmo resultado de verificação)
+
+    end
+end
+
+
+#===comentarios===#
+# Get.Elementbyxpach(//*[@id="rp1_edt"]/table/tbody/tr[2]/td/form/table/tbody/tr[1]/td[2]/input). click_button\
+#expect(page).to have_content 'Benner WES 2006.181.1.81teste' 
