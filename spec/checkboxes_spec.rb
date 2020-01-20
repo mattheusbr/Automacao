d5cd32fe04fcfaeae7d9413489dc70a01a2871c7
Matheus Ferreira Bruno <matheusferreirabruno@hotmail.com>
2020-01-20 11:18:11 -0300

    [estudo] marcando checkbox

--- spec/checkboxes_spec.rb
+++ spec/checkboxes_spec.rb
@@ -1,10 +1,19 @@
-describe 'Caixa de seleção' do 
+describe 'Caixa de seleção', :checkboxes do 
     before(:each) do
         visit 'http://training-wheels-protocol.herokuapp.com/checkboxes'
     end
     
     it 'Marca opção' do
-        
+        check('thor')
+    end
+
+    it 'Desmarcas opção' do
+        uncheck('thor')
+    end
+    
+    it 'Marcando/Desmarcando opção file set true' do
+        find('input[value=cap]').set(true)
+        find('input[value=ant-man]').set(false)
     end
 
     after(:each) do
--- spec/gmail_spec.rb
+++ spec/gmail_spec.rb
@@ -15,34 +15,11 @@ describe 'Automatização funções Gmail', :Gmail do
     end
     
     it 'Excluir Emails' do
-        modulo = find('.aKz')
-        modulo.find(text: 'Atualizações').click
-
-
-
-        #click_on(class: 'ZFr60d CeoRYc')
-        #find_element(:xpath,'//*[@id="identifierNext"]/span').click()
-        
-        
-        #fill_in 'password', with: '@'
-        #click_button 'identifierNext'
-
-        #Pergunta se o elemento #flash está visivel após o login
-        #expect(find('#:2r').visible?).to be true
-        
-        #Verifica se o texto do elemento mostrado na pagina contem essa parte
-        #expect(find('#flash').text).to include 'Olá, Tony Stark. Você acessou a área logada!'
-
-        #Verifica se o texto no elemento contem essa parte
-        #expect(find("#flash")).to have_content 'Olá, Tony Stark. Você acessou a área logada!'
-    
-    
-        #have_content | include (da o mesmo resultado de verificação)
+       # modulo = find(:xpath //*[@id=":2q"])
+        #modulo.click
+        #sleep 4
+        #modulo.first(text: 'Promoções').click
 
     end
 end
 
-
-#===comentarios===#
-# Get.Elementbyxpach(//*[@id="rp1_edt"]/table/tbody/tr[2]/td/form/table/tbody/tr[1]/td[2]/input). click_button\
-#expect(page).to have_content 'Benner WES 2006.181.1.81teste' 
