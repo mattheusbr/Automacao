29b0f123f357629c60d1bf98256aefff8ec65c67
Matheus Ferreira Bruno <matheusferreirabruno@hotmail.com>
2020-02-03 16:23:19 -0300

    [estudo]Usando IDs dinãmicos e CSS Select com partes do ID

---
+++ spec/iddinamico_spec.rb
@@ -0,0 +1,16 @@
+describe 'IDs Dinâmicos', :dinamico do
+    
+    before(:each) do
+        visit 'http://training-wheels-protocol.herokuapp.com/access'
+    end
+
+    it 'Cadastro IDs dinâmicos' do
+        find('input[id$=UsernameInput]').set 'teste'
+        find('input[id^=PasswordInput]').set 'teste'
+        click_link 'Criar Conta'
+
+        expect(find('#result')).to have_content 'Dados enviados. Aguarde aprovação do seu cadastro!'
+        sleep 1.5
+    end
+
+end
